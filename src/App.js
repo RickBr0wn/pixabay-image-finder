9f00295b911bdd4c57213a8a4ffce1bd768b1f5a
RiCK BROWN <ricky.brown.00@gmail.com>
2018-05-17 14:05:54 +0100

    Adding Components (NavBar & Search)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import TextField from 'material-ui/TextField/TextField'
+
+// Components
+import NavBar from './Components/NavBar/NavBar'
+import Search from './Components/Search/Search'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        HELLO
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+          <Search />
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
